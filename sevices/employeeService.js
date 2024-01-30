0c1f53b0985858bf8591f358f524574cb88e0d61
unknown <gauri.deshpande22@rediffmail.com>
2024-01-30 15:52:44 +0530

    Added Update service

---
+++ sevices/employeeService.js
@@ -0,0 +1,20 @@
+const User = require("../models/user");
+
+
+const update = async (data) => {
+    try{
+        if(data.email || data.password){
+            console.log("Should not update email or password.");
+            return;
+        }
+        const user = User.findByIdAndUpdate(data.id, data, {new:true}).exec();
+        return user;
+    }catch(err){
+        console.log(err);
+        return err;
+    }
+};
+
+module.exports = {
+    update
+}
